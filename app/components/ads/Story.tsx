796433065538582a7e5f1961c5f6d459910ea699
sourabh-bhatt <sourabhbhatt825@gmail.com>
2024-11-17 16:48:15 +0530

    added navbar and Story

---
+++ app/components/ads/Story.tsx
@@ -0,0 +1,7 @@
+export default function Story() {
+    return (
+        <div>
+            <h1>Story in capitalised format as function Story</h1>
+        </div>
+    )
+}
---
+++ app/components/common/navbar.tsx
@@ -0,0 +1,7 @@
+export default function navbar() {
+    return (
+        <div>
+            <h1>navbar in small letter as function navbar</h1>
+        </div>
+    )
+}
